5302d9ccc3e08ffeb3838190803f4fa11a8b0ee2
Jerry Owusu <65233860+jerryowusu@users.noreply.github.com>
2021-12-04 23:17:38 -0800

    ROUTES  changed to Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { ProductDescriptionComponent } from './product-description/product-descr
 import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
-import { RouterModule, ROUTES } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes= [
  { path: 'products', component: ProductListComponent},
